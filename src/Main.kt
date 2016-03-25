1fa18fbfb7a0bedbe41d95b38e9bd16ca2752345
Allen Garvey <allengarvey@hotmail.com>
2016-03-25 00:05:21 -0400

    add timer to midi parsing

--- src/Main.kt
+++ src/Main.kt
@@ -12,8 +12,11 @@ fun main(args: Array<String>) {
     //val player = MidiPlayer()
     //player.play()
     val parser = MidiParser()
-    val midiFileName : String = System.getProperty("user.home") + "/Desktop/test4.mid"
+    val midiFileName : String = System.getProperty("user.home") + "/Desktop/test.mid"
     println("Midi file format is: " + MidiSystem.getMidiFileFormat(File(midiFileName)).type )
 
+    val start = System.currentTimeMillis()
     parser.parse(midiFileName)
+    val end = System.currentTimeMillis()
+    println("Midi parsing took: " + (end  - start) + " milliseconds")
 }
