420887fdcbc13b5d6963766b4fd3da1d6974f951
Allen Garvey <allengarvey@hotmail.com>
2016-03-24 22:57:29 -0400

    add test for midi parser

--- src/Main.kt
+++ src/Main.kt
@@ -4,8 +4,16 @@
 
 package aged.midi
 
+import java.io.File
+import javax.sound.midi.MidiSystem
+
 fun main(args: Array<String>) {
     println("hello")
-    val player = MidiPlayer()
-    player.play()
+    //val player = MidiPlayer()
+    //player.play()
+    val parser = MidiParser()
+    val midiFileName : String = "/Users/allen/Desktop/test4.mid"
+    println("Midi file format is: " + MidiSystem.getMidiFileFormat(File(midiFileName)) )
+
+    parser.parse(midiFileName)
 }
