2575930b8ff5270c0ec87ccd0d8779bbfae36007
Allen Garvey <allengarvey@hotmail.com>
2016-03-24 22:57:18 -0400

    refactor parse function to take midi filename

--- src/MidiParser.kt
+++ src/MidiParser.kt
@@ -2,26 +2,44 @@
  * Created by Allen X on 3/23/16.
  */
 
+
+
 package aged.midi
 
+import aged.util.CountedNoteSet
 import java.io.File
 import javax.sound.midi.*
+import  com.sun.media.sound.*
 
 class MidiParser{
 
-    val NOTE_ON : Int = 0x90
-    val NOTE_OFF : Int = 0x80
-    val NOTE_NAMES : Array<String> = arrayOf("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+    val NOTE_ON : Int = ShortMessage.NOTE_ON
+    val NOTE_OFF : Int = ShortMessage.NOTE_OFF
+    val NOTE_NAMES : Array<String> = arrayOf("C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B")
 
-    public fun parse(){
+
+    //adapted from: http://stackoverflow.com/questions/3850688/reading-midi-files-in-java
+    fun parse(fileName : String){
         try {
-            val sequence: Sequence = MidiSystem.getSequence(File("test.mid"))
-
+            val sequence: Sequence = MidiSystem.getSequence(File(fileName))
+            val noteSet : CountedNoteSet = CountedNoteSet()
             var trackNumber: Int = 0
-            for (track: Track in  sequence.getTracks()) {
+            val tracks : Array<Track> = sequence.tracks
+            println("There are " + tracks.size  + " tracks")
+
+           //not iterating over track 2 for some reason
+            val messageTest = tracks[1].get(2).message
+            if(messageTest is ShortMessage){
+                val sm : ShortMessage = messageTest
+                println(sm.data1)
+            }
+
+
+            for (track : Track in tracks) {
                 trackNumber++
                 println("Track " + trackNumber + ": size = " + track.size() + "\n")
                 for (i: Int in 0..track.size()) {
+                    println("event " + i + " starting")
                     val event: MidiEvent = track.get(i)
                     print("@" + event.getTick() + " ")
                     val message: MidiMessage = event.getMessage()
@@ -33,25 +51,34 @@ class MidiParser{
                             val octave: Int = (key / 12) - 1
                             val note: Int = key % 12
                             val noteName: String = NOTE_NAMES[note]
+                            noteSet.add(noteName)
                             val velocity: Int = sm.getData2()
                             println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity)
-                        } else if (sm.getCommand() == NOTE_OFF) {
+                        }
+                        else if (sm.getCommand() == NOTE_OFF) {
                             val key: Int = sm.getData1()
                             val octave: Int = (key / 12) - 1
                             val note: Int = key % 12
                             val noteName: String = NOTE_NAMES[note]
                             val velocity: Int = sm.getData2()
                             println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity)
-                        } else {
+                        }
+                        else {
                             println("Command:" + sm.getCommand())
                         }
-                    } else {
-                        println("Other message: " + message.toString())
                     }
-                }
+                    else {
+                        println("Other message: " + message.toString())
+                        if(message is MetaMessage){
+                            val meta : MetaMessage = message
+                            println(meta.data[0])
+                        }
+                    }
 
+                }
                 println()
             }
+            println(noteSet.collection)
         }
         catch(e : Exception){
             println(e.message)
