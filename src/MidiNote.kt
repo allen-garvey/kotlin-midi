d02dca8572fac575ab1b9351a1697bf8032e189a
Allen Garvey <allengarvey@hotmail.com>
2016-03-30 22:07:06 -0400

    change midi note static properties to computed properties

--- src/MidiNote.kt
+++ src/MidiNote.kt
@@ -6,12 +6,15 @@ import javax.sound.midi.ShortMessage
  * Created by Allen X on 3/28/16.
  */
 
-class MidiNote(val keyNum : Int, val velocity : Int = 60){
+class MidiNote(var keyNum : Int, val velocity : Int = 60){
     val NOTE_NAMES : Array<String> = arrayOf("C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B")
 
-    val octave: Int = (keyNum / 12) - 1
-    val note: Int = keyNum % 12
-    val noteName: String = NOTE_NAMES[note]
+    val octave: Int
+        get() = (keyNum / 12) - 1
+    val note: Int
+        get() = keyNum % 12
+    val noteName: String
+        get() = NOTE_NAMES[note]
 
     constructor(sm : ShortMessage) : this(sm.data1, sm.data2){
 
