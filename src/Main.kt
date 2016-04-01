418c123b04f32eb8d9ee1346d59aa0aefb66817c
Allen Garvey <allengarvey@hotmail.com>
2016-03-31 22:50:24 -0400

    add jfugue example

--- src/Main.kt
+++ src/Main.kt
@@ -6,17 +6,28 @@ package aged.midi
 
 import java.io.File
 import javax.sound.midi.MidiSystem
+import org.jfugue.midi.*
+import org.jfugue.pattern.*
+import org.jfugue.player.*
+import org.staccato.*
+
+
 
 fun main(args: Array<String>) {
-    println("hello")
-    //val player = MidiPlayer()
-    //player.play()
+    val start = System.currentTimeMillis()
+
+    /*
     val parser = MidiParser()
     val midiFileName : String = System.getProperty("user.home") + "/Desktop/test.mid"
     println("Midi file format is: " + MidiSystem.getMidiFileFormat(File(midiFileName)).type )
-
-    val start = System.currentTimeMillis()
     parser.parse(midiFileName)
+    */
+
+
+    val pattern = MidiFileManager.loadPatternFromMidi(File(System.getProperty("user.home") + "/Desktop/test.mid"))
+    println(pattern)
+
+    
     val end = System.currentTimeMillis()
-    println("Midi parsing took: " + (end  - start) + " milliseconds")
+    println("Program time: " + (end  - start) + " milliseconds")
 }
