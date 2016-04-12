97505f82f38ec1951d2bd041385376520f516a86
Allen Garvey <allengarvey@hotmail.com>
2016-04-11 22:55:54 -0400

    fix indentation

--- src/MidiParser.kt
+++ src/MidiParser.kt
@@ -23,52 +23,52 @@ class MidiParser{
 
     //adapted from: http://stackoverflow.com/questions/3850688/reading-midi-files-in-java
     fun parse(sequence : Sequence){
-            if(sequence.divisionType == Sequence.PPQ){
-                println("Sequence timing is PPQ")
-                println("Resolution is " + sequence.resolution + " ticks per quarter")
-            }
-            val noteSet : CountedNoteSet = CountedNoteSet()
-            var trackNumber: Int = 0
-            val tracks : Array<Track> = sequence.tracks
-            println("There are " + tracks.size  + " tracks")
+        if(sequence.divisionType == Sequence.PPQ){
+            println("Sequence timing is PPQ")
+            println("Resolution is " + sequence.resolution + " ticks per quarter")
+        }
+        val noteSet : CountedNoteSet = CountedNoteSet()
+        var trackNumber: Int = 0
+        val tracks : Array<Track> = sequence.tracks
+        println("There are " + tracks.size  + " tracks")
 
-            for (track : Track in tracks) {
-                trackNumber++
-                println("Track " + trackNumber + " has " + track.size() + " midi events\n")
-                for (i: Int in 0..track.size() - 1) {
-                    val event: MidiEvent = track.get(i)
-                    val message: MidiMessage = event.getMessage()
-                    if (message is ShortMessage) {
-                        val sm: ShortMessage = message
-                        if (sm.getCommand() == NOTE_ON) {
-                            val midiNote = MidiNote(sm)
-                            noteSet.add(midiNote)
-                            println(midiNote.toString() + " started at " + event.tick + " ticks")
-                            //println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity)
-                        }
-                        else if (sm.getCommand() == NOTE_OFF) {
-                            val midiNote = MidiNote(sm)
-                            //println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity)
-                        }
-
+        for (track : Track in tracks) {
+            trackNumber++
+            println("Track " + trackNumber + " has " + track.size() + " midi events\n")
+            for (i: Int in 0..track.size() - 1) {
+                val event: MidiEvent = track.get(i)
+                val message: MidiMessage = event.getMessage()
+                if (message is ShortMessage) {
+                    val sm: ShortMessage = message
+                    if (sm.getCommand() == NOTE_ON) {
+                        val midiNote = MidiNote(sm)
+                        noteSet.add(midiNote)
+                        println(midiNote.toString() + " started at " + event.tick + " ticks")
+                        //println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity)
                     }
-                    else if(message is MetaMessage){
-                        val timeSignature : TimeSignature? = decodeTimeSignature(message)
-                        if(timeSignature is TimeSignature) println(timeSignature)
+                    else if (sm.getCommand() == NOTE_OFF) {
+                        val midiNote = MidiNote(sm)
+                        //println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity)
                     }
-
 
                 }
+                else if(message is MetaMessage){
+                    val timeSignature : TimeSignature? = decodeTimeSignature(message)
+                    if(timeSignature is TimeSignature) println(timeSignature)
+                }
+
+
             }
-            val sortedNoteCollection = noteSet.sortedCollection
-            for(noteSetEntry : CountedNoteSetEntry in sortedNoteCollection){
-                println(noteSetEntry)
-            }
-            println("\nNormalized collection ")
-            val normalizedSortedNoteCollection = noteSet.normalizedSortedCollection()
-            for(noteSetEntry2 : CountedNoteSetEntry in normalizedSortedNoteCollection){
-               println(noteSetEntry2)
-            }
+        }
+        val sortedNoteCollection = noteSet.sortedCollection
+        for(noteSetEntry : CountedNoteSetEntry in sortedNoteCollection){
+            println(noteSetEntry)
+        }
+        println("\nNormalized collection ")
+        val normalizedSortedNoteCollection = noteSet.normalizedSortedCollection()
+        for(noteSetEntry2 : CountedNoteSetEntry in normalizedSortedNoteCollection){
+           println(noteSetEntry2)
+        }
 
 
     }
